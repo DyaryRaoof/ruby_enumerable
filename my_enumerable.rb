9e9046f4ecaaab67ae9a0f340fbe5f9abf5b6523
mike2611 <mpuentes2611@gmail.com>
2021-12-09 13:35:36 -0600

    Add any? method an filter method to my_enumerable.rb

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -8,8 +8,20 @@ module MyEnumerable
     end
 
     def any?
+        array = []
+        @list.each do |li|
+            array.push( yield li)
+        end
+        array.any?
     end
 
-    def filter?
+    def filter
+        array = []
+        @list.each do |li|
+            if yield li
+                array.push(li)
+            end
+        end
+        array
     end
 end
